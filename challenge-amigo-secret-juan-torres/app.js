9d7018d470fce8872982df1d9191147f93dd7a84
juantorresun <117603005+juantorresun@users.noreply.github.com>
2025-07-30 12:31:40 -0500

    listar_amigos

--- challenge-amigo-secret-juan-torres/app.js
+++ challenge-amigo-secret-juan-torres/app.js
@@ -17,4 +17,20 @@ function agregarAmigo() {
     
     // Agrega el nombre al arreglo de amigos
     amigos.push(nombre);
+    mostrarAmigos(); // Llama a la función para mostrar la lista de amigos
 }
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpia la lista antes de mostrar los amigos
+    // Recorre el arreglo de amigos y los muestra en la lista
+    amigos.forEach(amigo => {
+        let li = document.createElement("li"); // Crea un nuevo elemento de lista
+        li.textContent = amigo; // Establece el texto del elemento de lista al nombre del amigo
+        lista.appendChild(li); // Agrega el elemento de lista a la lista en el DOM
+    }
+    );
+    
+
+}
+
